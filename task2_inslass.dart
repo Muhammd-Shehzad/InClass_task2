3a82e88c78f6631d974c96f548765444f1b517ca
Muhammd-Shehzad <127086042+Muhammd-Shehzad@users.noreply.github.com>
2024-03-31 20:40:05 +0500

    Add files via upload

---
+++ task2_inslass.dart
@@ -0,0 +1,16 @@
+void main(){
+
+var num1=10;
+var num2=30;
+
+if(num1 == num2 ){
+  print('Both are Equal Number');
+}else if( num1 < num2){
+  print('num1 is Smaller than num2');
+}else if( num1 > num2){
+  print('num2 is Grater than naum 2');
+}
+
+
+
+}
